e8a14ca702e95ee7b3df188f038582a3889808dd
Samuel Eubanks <swamulism@gmail.com>
2018-03-02 21:55:21 -0700

    seperated things into functions

--- src/map.rs
+++ src/map.rs
@@ -19,41 +19,49 @@ pub struct Island {
 
 impl Island {
     pub fn new() -> Self {
-        // https://doc.rust-lang.org/1.0.0/rand/distributions/normal/struct.Normal.html
-        let mut normal = distributions::normal::Normal::new(10.0, 2.0);
-        let mut island_size = normal.sample(&mut thread_rng());
-        while island_size <= 2.0 {
-            island_size = normal.sample(&mut thread_rng());
-        }
-        let island_size = island_size as usize;
+        let island_size = generate_island_size();
+        let perlin_arr = generate_perlin(island_size, 0.2);
         let mut temp_tiles = vec![vec![TileType::Air; island_size]; island_size];
-
-        // Docs here https://docs.rs/noise/0.4.1/noise/struct.Perlin.html
-        let perlin = Perlin::new();
-        // maybe need to use Perlin::set_seed(self, seed: usize)
-        let mut xpos = 0.0;
-        let mut ypos = 0.0;
         for i in 0..temp_tiles.len() {
             for j in 0..temp_tiles[i].len() {
-                let num = perlin.get([xpos, ypos]);
+                let num = perlin_arr[i][j];
                 if num < 0.0 {
                     temp_tiles[i][j] = TileType::Grass;
                 }
                 else {
                     temp_tiles[i][j] = TileType::Water;
                 }
-                println!("| {:?} |", num);
-                xpos += 0.2;
-                ypos += 0.2;
             }
         }
         Island {
             tiles: temp_tiles
         }
-        // Island {
-        //     tiles: vec![vec![TileType::Air; 10]; 10]
-        // }
     }
+}
+
+fn generate_perlin(size: usize, step: f32) -> Vec<Vec<f32>> {
+    let perlin = Perlin::new();
+    let mut xpos = 0.0;
+    let mut ypos = 0.0;
+    let mut perlin_arr = vec![vec![0.0; size]; size];
+    for i in 0..perlin_arr.len() {
+        for j in 0..perlin_arr[i].len() {
+            perlin_arr[i][j] = perlin.get([xpos, ypos]);
+            xpos += step;
+            ypos += step;
+        }
+    }
+    perlin_arr
+}
+
+fn generate_island_size() -> usize {
+    let mut normal = distributions::normal::Normal::new(10.0, 2.0);
+    let mut island_size = normal.sample(&mut thread_rng());
+    while island_size <= 2.0 && island_size >= 50.0 {
+        island_size = normal.sample(&mut thread_rng());
+    }
+    let island_size = island_size as usize;
+    island_size
 }
 
 // pub struct Map {
