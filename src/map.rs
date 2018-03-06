2a1ea6717b8d28478dcf147ff726767bff69d4d1
Samuel Eubanks <swamulism@gmail.com>
2018-03-05 17:53:50 -0700

    made hardcoded stuff const

--- src/map.rs
+++ src/map.rs
@@ -2,6 +2,12 @@ use noise::*;
 use rand::{distributions, thread_rng};
 use rand::distributions::Sample;
 
+const ISLAND_MEAN: f32 = 10.0;
+const ISLAND_STANDERD_DEV: f32 = 2.0;
+const ISLAND_LOWERBOUND: f32 = 2.0;
+const ISLAND_UPPERBOUND: f32 = 50.0;
+const ISLAND_STEP: f32 = 0.2;
+
 #[derive(Copy, Clone)]
 pub enum TileType {
     Air,
@@ -16,7 +22,7 @@ pub struct Island {
 impl Island {
     pub fn new() -> Self {
         let island_size = generate_island_size();
-        let perlin_arr = generate_perlin(island_size, 0.2);
+        let perlin_arr = generate_perlin(island_size, ISLAND_STEP);
         let mut temp_tiles = vec![vec![TileType::Air; island_size]; island_size];
         for i in 0..temp_tiles.len() {
             for j in 0..temp_tiles[i].len() {
@@ -48,23 +54,23 @@ fn generate_perlin(size: usize, step: f32) -> Vec<Vec<f32>> {
 }
 
 fn generate_island_size() -> usize {
-    let mut normal = distributions::normal::Normal::new(10.0, 2.0);
+    let mut normal = distributions::normal::Normal::new(ISLAND_MEAN, ISLAND_STANDERD_DEV);
     let mut island_size = normal.sample(&mut thread_rng());
-    while island_size <= 2.0 && island_size >= 50.0 {
+    while island_size <= ISLAND_LOWERBOUND && island_size >= ISLAND_UPPERBOUND {
         island_size = normal.sample(&mut thread_rng());
     }
     let island_size = island_size as usize;
     island_size
 }
 
-pub struct Map {
-    pub tiles: Vec<Vec<TileType>>,
-}
-
-impl Map {
-    pub fn new() -> Self {
-        let worley: Worley<f32> = Worley::new();
-        let map_tiles = vec![vec![TileType::Air; 1]; 1];
-        Map { tiles: map_tiles }
-    }
-}
+// pub struct Map {
+//     pub tiles: Vec<Vec<TileType>>,
+// }
+//
+// impl Map {
+//     pub fn new() -> Self {
+//         let worley: Worley<f32> = Worley::new();
+//         let map_tiles = vec![vec![TileType::Air; 1]; 1];
+//         Map { tiles: map_tiles }
+//     }
+// }
